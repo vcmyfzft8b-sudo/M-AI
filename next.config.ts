bde44caa0c2ec2d09a6acef1251d4e0a6e2bcae2
NV <vcmyfzft8b@privaterelay.appleid.com>
2026-05-24 13:47:49 +0200

    Polish landing page slideshow and scroll reveal

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: process.cwd(),
+  images: {
+    qualities: [75, 92],
+  },
 };
 
 export default withSentryConfig(nextConfig, {
